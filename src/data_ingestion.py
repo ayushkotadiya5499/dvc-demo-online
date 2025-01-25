032e73f53135f3e7c3bc182a02a9537f8084e587
ayushkotadiya5499 <142094873+ayushkotadiya5499@users.noreply.github.com>
2025-01-25 18:31:51 +0530

    add first exp

---
+++ src/data_ingestion.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import os
+
+def load_data(url):
+    df=pd.read_csv(url)
+    return df
+
+def drop_columns(df,column_name):
+    df=df.drop(columns=[column_name],axis=0)
+    return df
+
+def main():
+    df=load_data('https://raw.githubusercontent.com/araj2/customer-database/refs/heads/master/Ecommerce%20Customers.csv')
+    df=df.iloc[:,3:]
+    df=df[df['Length of Membership']>3]
+    data='data'
+    df.to_csv(os.path.join(data,'customer.csv'))
+
+if __name__=='__main__':
+    main()
